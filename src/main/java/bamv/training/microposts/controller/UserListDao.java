12b5144c3dd371c9b78a0be1d105980f472d241a
kzk3y <91412581+kzk3y@users.noreply.github.com>
2023-09-14 20:54:28 +0900

    UserListDao.followで、重複フォローを防ぐ処理を追加。すでに存在するフォロワー・フォロワーの組であればINSERTせずにreturnする。

--- src/main/java/bamv/training/microposts/controller/UserListDao.java
+++ src/main/java/bamv/training/microposts/controller/UserListDao.java
@@ -31,13 +31,18 @@ public class UserListDao {
         return userList;
     }
 
-    //String newMicropostId = sequenceService.issueSequence("micropost_id");
+    //INSERTの実行前に同一userId, followeeIdでレコードがあれば何もせずにreturnする
     public int follow(String userId, String followeeId) {
+        String checkDuplicateSql = "SELECT COUNT(*) FROM t_follow WHERE following_user_id = ? AND followed_user_id = ?";
+        int count = jdbcTemplate.queryForObject(checkDuplicateSql, Integer.class, userId, followeeId);
+        if (count != 0) //すでにフォローしている
+            return 0;
+
         String newFollowId = sequenceService.issueSequence("follow_id");
         int number = jdbcTemplate.update("INSERT INTO t_follow (follow_id, following_user_id, followed_user_id) VALUES (?, ?, ?)", newFollowId, userId, followeeId);
         return number;
     }
-    //アンフォローのメソッド
+
     public int unfollow(String userId, String followeeId) {
         int number = jdbcTemplate.update("DELETE FROM t_follow WHERE following_user_id = ? AND followed_user_id = ?", userId, followeeId);
         return number;
